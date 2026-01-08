648f3301358db89ba3b1c07e0cc480e7841288a1
v.oleynikov <vasily.oleynikov@flant.com>
2026-01-08 13:10:17 +0300

    Small fix

--- langed/init.sh
+++ langed/init.sh
@@ -2,5 +2,5 @@
 
 /langed/.pyenv/versions/langed/bin/python3 manage.py makemigrations
 /langed/.pyenv/versions/langed/bin/python3 manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@dashboard.dashboard', 'qwerty')" | /langed/.pyenv/versions/langed/bin/python3 manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@langed.langed', 'qwerty')" | /langed/.pyenv/versions/langed/bin/python3 manage.py shell
 /langed/.pyenv/versions/langed/bin/uwsgi --ini /langed/uwsgi/langed.main
